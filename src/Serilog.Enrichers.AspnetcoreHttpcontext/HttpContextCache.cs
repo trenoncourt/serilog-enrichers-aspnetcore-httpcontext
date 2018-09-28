c5c7c1e217d182b2c77a3204e5486d3842e5b7ef
Łukasz Szydłowski <lukasz.szydlowski@eshare.co.uk>
2018-09-28 10:53:16 +0200

    Additional http request properties added

--- src/Serilog.Enrichers.AspnetcoreHttpcontext/HttpContextCache.cs
+++ src/Serilog.Enrichers.AspnetcoreHttpcontext/HttpContextCache.cs
@@ -11,11 +11,23 @@ namespace Serilog.Enrichers.AspnetcoreHttpcontext
 
         public string Path { get; set; }
 
+        public bool IsHttps { get; set; }
+
+        public string Scheme { get; set; }
+
         public string Method { get; set; }
+
+        public string ContentType { get; set; }
+
+        public string Protocol { get; set; }
 
         public string QueryString { get; set; }
 
+        public Dictionary<string, string> Query { get; set; }
+
         public Dictionary<string, string> Headers { get; set; }
+
+        public Dictionary<string, string> Cookies { get; set; }
 
         public string Body { get; set; }
     }
